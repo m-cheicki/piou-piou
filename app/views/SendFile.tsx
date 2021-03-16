c15c6b9f8436ea29fd7033fec305032128b5956c
m-cheicki <mcheicki.dev@gmail.com>
2021-03-16 20:37:54 +0100

    🍻 untested file encoding

--- app/views/SendFile.tsx
+++ app/views/SendFile.tsx
@@ -67,26 +67,62 @@ export default class SendFile extends Component<any, any> {
             this.setState({ enableConvert: true })
         }
     }
+    
+    private _encode_base64_to_signal = (base64: string, sample_rate: number, bloc_duration: number) => {
+        const base64_values: number[] = Base64.base64_to_array(base64)
 
-    private _onConvert = (audioType: string) => {
-        const base64: string = this.state.base64Data
-        console.log('Start converting', base64)
-        if (base64) {
-            console.log('Converting base64 to data array')
-            let bytes: number[] = []
+        const freqs: number[] = linspace(100, 600, 6)
+        const freq_porteuse = 50
+        const amp: number = 90
 
-            for (let i = 0; i < base64.length; i++) {
-                const char: string = base64[i]
-                const val: number = Base64.keyStr.indexOf(char)
-                bytes.push(val)
+        let signal: number[] = []
+
+        for (let i = 0; i < base64_values.length; i++) {
+            const val = base64_values[i]
+            const binary_arr: number[] = BitSeparator.split(val, 1, 6)
+
+            let bloc_signal: number[] = SignalBuilder.generateSignal(freq_porteuse, 0, bloc_duration, sample_rate, amp)
+
+            for (let j = 0; j < freqs.length; j++) {
+                if (binary_arr[j] != 0) {
+                    const freq: number = freqs[j]
+                    const freq_i_signal: number[] = SignalBuilder.generateSignal(freq, 0, bloc_duration, sample_rate, amp)
+                    bloc_signal = concat(bloc_signal, freq_i_signal)
+                }
             }
 
-            console.log('Generating audio')
-            // this._generateAudio(bytes, audioType)
+            bloc_signal = normalize(bloc_signal, amp)
+            signal = [...signal, ...bloc_signal]
+        }
+
+        return signal
+    }
+
+    public encode = (message: string) => {
+        const SAMPLE_RATE = 44100
+        const BLOC_DURATION = 0.2
+        const BIT_PER_SAMPLE = 16
+
+        const message_b64: string = Base64.encode(message)
+
+        const signal: number[] = this._encode_base64_to_signal(message_b64, SAMPLE_RATE, BLOC_DURATION)
+
+        const sound = WAV.encode(signal, 1, SAMPLE_RATE, BIT_PER_SAMPLE)
+
+        fs.writeFileSync('tmp/test_encode.wav', sound, { encoding: null })
+    }
+
+    private _onConvert = () => {
+        const base64: string = this.state.base64Data
+        
+        console.log('Start converting : ', base64.length)
+        
+        if (base64) {
+            console.log('Encoding....')
+            this.encode(base64)
         }
     }
 
-    
     private _onDeleteFile = async () => {
         await this._getFileInStorage()
     }
